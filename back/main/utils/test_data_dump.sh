f9dc5e12c5c66aacbd907596374c56f1d6a696e5
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2025-01-18 13:58:57 +0300

    added some test data for course module

--- back/main/utils/test_data_dump.sh
+++ back/main/utils/test_data_dump.sh
@@ -8,3 +8,9 @@ python manage.py dumpdata --format=yaml -o apps/org/fixtures/Org.yaml           
 python manage.py dumpdata --format=yaml -o apps/org/fixtures/OrgUser.yaml               org.OrgUser 
 python manage.py dumpdata --format=yaml -o apps/org/fixtures/OrgUserGroup.yaml          org.OrgUserGroup
 python manage.py dumpdata --format=yaml -o apps/org/fixtures/OrgUserInOrgUserGroup.yaml org.OrgUserInOrgUserGroup
+# Course 
+rm apps/course/fixtures/*
+python manage.py dumpdata --format=yaml -o apps/course/fixtures/Course.yaml             course.Course
+python manage.py dumpdata --format=yaml -o apps/course/fixtures/CourseChapter.yaml      course.CourseChapter
+python manage.py dumpdata --format=yaml -o apps/course/fixtures/Lesson.yaml             course.Lesson
+python manage.py dumpdata --format=yaml -o apps/course/fixtures/LessonBlock.yaml        course.LessonBlock
